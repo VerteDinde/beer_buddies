9dc83c19dbf9be849b667e69554081d64652121a
F Grayson Meindl <meindlgrayson@gmail.com>
2017-03-02 11:31:16 -0800

    reuslts displayed from high score to low

--- js/about.js
+++ js/about.js
@@ -1,45 +1,53 @@
 'use strict';
 
 var allUsers = [];
-var winnerCircle = [];
-var winnerScore = [];
+// var winnerCircle = [];
+// var winnerScore = [];
+
+function myEpiphany (a,b) {
+  return b.score - a.score;
+}
 
 function retrieveUsers() {
   var retrievedUsers = localStorage.getItem('userData');
   allUsers = JSON.parse(retrievedUsers);
 }
 
-function winners() {
-  for (var i = 0; i < allUsers.length; i++) {
-    winnerScore.push(allUsers[i].score);
-  }
-  winnerScore.sort();
-  for (var j = 0; j < winnerScore.length; j++) {
-    console.log(winnerScore[j]);
-    for (var k = 0; k < allUsers.length; k++) {
-      if (winnerScore[j] === allUsers[k].score) {
-        winnerCircle.push(allUsers[k]);
-      }
-    }
-  }
-}
+
+
+// function winners() {
+//   for (var i = 0; i < allUsers.length; i++) {
+//     winnerScore.push(allUsers[i].score);
+//   }
+//   winnerScore.sort();
+//   for (var j = 0; j < winnerScore.length; j++) {
+//     console.log(winnerScore[j]);
+//     for (var k = 0; k < allUsers.length; k++) {
+//       if (winnerScore[j] === allUsers[k].score) {
+//         winnerCircle.push(allUsers[k]);
+
+//       }
+//     }
+//   }
+// }
 
 // Run initial functions to populate user data
 retrieveUsers();
-winners();
+// winners();
+allUsers.sort(myEpiphany);
 
 //Display Results in Table
 var tableName = document.getElementById('table-name');
 var tableResults = document.getElementById('table-points');
 
 function onFinal() {
-  for (var i = 0; i < winnerCircle.length; i++) {
+  for (var i = 0; i < allUsers.length; i++) {
     var winnerName = document.createElement('th');
-    winnerName.textContent = winnerCircle[i].name;
+    winnerName.textContent = allUsers[i].name;
     tableName.appendChild(winnerName);
 
     var winnerPoints = document.createElement('td');
-    winnerPoints.textContent = winnerCircle[i].score;
+    winnerPoints.textContent = allUsers[i].score;
     tableResults.appendChild(winnerPoints);
   }
 }
